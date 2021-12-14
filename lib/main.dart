57071f9918e4526013403e97bb4ba1f81af1449b
fedniko <fednik2011@gmail.com>
2021-12-14 12:28:23 +0900

    feat: add login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,161 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-void main() {
-  runApp(const MyApp());
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
+
+Future<void> main() async {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      fontFamily: 'Roboto',
+      colorScheme:
+          ColorScheme.fromSwatch().copyWith(secondary: const Color(0xFF6667AB)),
+      primaryColor: const Color(0xFFf8f8f8),
+      canvasColor: const Color(0xffFFFFFF),
+      unselectedWidgetColor: const Color(0xFF6667AB),
+      toggleableActiveColor: const Color(0xFF6667AB),
+      indicatorColor: const Color(0xFF6667AB),
+      appBarTheme: const AppBarTheme(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Color(0xFFf9f9f9),
+        iconTheme: IconThemeData(color: Color(0xffa1a1a1)),
+        titleTextStyle: (TextStyle(
+            color: Color(0xFF2F2F2F),
+            fontWeight: FontWeight.normal,
+            fontSize: 20,
+            fontFeatures: [
+              FontFeature.enable('pnum'),
+              FontFeature.enable('lnum')
+            ])),
+      ),
+      textTheme: const TextTheme(
+        bodyText1: TextStyle(
+            color: Color(0xff2F2F2F),
+            fontSize: 14,
+            fontFeatures: [
+              FontFeature.enable('pnum'),
+              FontFeature.enable('lnum')
+            ]),
+        bodyText2: TextStyle(
+            color: Color(0xff2F2F2F),
+            fontSize: 14,
+            fontFeatures: [
+              FontFeature.enable('pnum'),
+              FontFeature.enable('lnum')
+            ]),
+        subtitle1: TextStyle(
+            color: Color(0xff2F2F2F),
+            fontSize: 14,
+            fontFeatures: [
+              FontFeature.enable('pnum'),
+              FontFeature.enable('lnum')
+            ]),
+      ),
+      pageTransitionsTheme: const PageTransitionsTheme(
+        builders: {
+          TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+          TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+        },
+      ),
+    ),
+    builder: (context, child) {
+      return ScrollConfiguration(
+        behavior: MyBehavior(),
+        child: child!,
+      );
+    },
+    home: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        body: Stack(children: [
+      SvgPicture.asset(
+        'assets/images/loginScreen.svg',
+        alignment: Alignment.center,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      Container(
+        margin: const EdgeInsets.symmetric(horizontal: 30),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                children: const [
+                  Text(
+                    'Работай на себя',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      height: 1.16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 22,
+                  ),
+                  Text(
+                    'Портал для самозанятых предоставляет удобный сервис для поиска работы самозанятым',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 14,
+                      height: 1.4,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(
+              height: 61,
+            ),
+            MaterialButton(
+                minWidth: double.infinity,
+                child: const Text(
+                  "НАЧАТЬ ПОИСК",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xffFFFFFF),
+                    fontSize: 14,
+                    height: 1.43,
+                  ),
+                ),
+                color: Theme.of(context).colorScheme.secondary,
+                height: 52,
+                shape: (RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                )),
+                onPressed: () {}),
+            const SizedBox(
+              height: 60,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ]));
   }
 }
